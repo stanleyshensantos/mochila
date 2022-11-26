bf2a8fbf3055a25d2aa4a553d733bc661d5eea40
Staney Shen <stanley_shen@hotmail.com>
2022-11-26 19:51:40 -0300

    https://github.com/stanleyshensantos/mochila

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 
 const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
 
 form.addEventListener("submit", (evento) => {
-    EventCounts.preventDefault()
+    evento.preventDefault()
 
-    console.log("funcionou")
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
 })
+
+function criaElemento(nome, quantidade){
+   const novoItem = document.createElement('li')
+   novoItem.classList.add("item")
+
+   const numeroItem = document.createElement('strong')
+   numeroItem.innerHTML = quantidade
+
+   novoItem.appendChild(numeroItem)
+   novoItem.innerHTML += nome
+
+   lista.appendChild(novoItem) 
+}
