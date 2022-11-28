600fc312b43be291588290814e458cb8fffad555
Staney Shen <stanley_shen@hotmail.com>
2022-11-27 22:46:55 -0300

    https://github.com/stanleyshensantos/mochila

--- js/main.js
+++ js/main.js
@@ -5,7 +5,11 @@ const lista = document.getElementById("lista")
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+    criaElemento(nome.value, quantidade.value)
+    nome.value = ""
+    quantidade.value = ""
 })
 
 function criaElemento(nome, quantidade){
@@ -19,4 +23,12 @@ function criaElemento(nome, quantidade){
    novoItem.innerHTML += nome
 
    lista.appendChild(novoItem) 
+
+   const itemAtual ={
+    "nome":nome,
+    "quantidade":quantidade
+   }
+
+   localStorage.setItem("item",itemAtual)
+   
 }
